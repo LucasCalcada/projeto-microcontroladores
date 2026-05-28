840d84c7b2ecd9e4898a89f947cbc9b042d60833
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-05-27 22:36:06 -0300

    chore: update motor pins

--- include/robotConfig.h
+++ include/robotConfig.h
@@ -2,7 +2,7 @@
 
 #include "motor.h"
 
-static Motor frontLeftMotor = {.pinA = 2, .pinB = 3, .reversed = false};
+static Motor frontLeftMotor = {.pinA = 8, .pinB = 9, .reversed = false};
 static Motor frontRightMotor = {.pinA = 6, .pinB = 7, .reversed = false};
 static Motor backRightMotor = {.pinA = 18, .pinB = 19, .reversed = false};
 static Motor backLeftMotor = {.pinA = 20, .pinB = 21, .reversed = false};
