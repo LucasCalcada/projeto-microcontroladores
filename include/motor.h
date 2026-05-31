60319e9552e9be81426a2d4cf0fe8794983b7964
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-05-30 23:21:23 -0300

    feat: update motor driver, add protection and better speed control

--- include/motor.h
+++ include/motor.h
@@ -2,12 +2,17 @@
 
 #include "pico/types.h"
 
+#define WRAP 12499
+#define DEADZONE 1000
+#define MAX_SPEED (WRAP / 2)
+
 typedef struct
 {
   uint pinA;
   uint pinB;
   bool reversed;
+  int currentSpeed;
 } Motor;
 
-void motor_setup(Motor motor);
-void motor_move(Motor motor, int speed);
+void motor_setup(Motor *motor);
+void motor_move(Motor *motor, int speed);
--- src/motor.c
+++ src/motor.c
@@ -3,41 +3,88 @@
 #include "hardware/gpio.h"
 #include "hardware/pwm.h"
 #include <stdlib.h>
+#include "pico/stdlib.h"
 
-void motor_setup(Motor motor)
+#define DEADTIME_US 50
+
+void motor_setup(Motor *motor)
 {
-  gpio_init(motor.pinA);
-  gpio_set_function(motor.pinA, GPIO_FUNC_PWM);
+  gpio_init(motor->pinA);
+  gpio_set_function(motor->pinA, GPIO_FUNC_PWM);
 
-  gpio_init(motor.pinB);
-  gpio_set_function(motor.pinB, GPIO_FUNC_PWM);
+  gpio_init(motor->pinB);
+  gpio_set_function(motor->pinB, GPIO_FUNC_PWM);
 
   pwm_config config = pwm_get_default_config();
   pwm_config_set_clkdiv(&config, 1.0f);
-  pwm_config_set_wrap(&config, 2000);
-  uint slice = pwm_gpio_to_slice_num(motor.pinA);
-  pwm_init(slice, &config, true);
+  pwm_config_set_wrap(&config, WRAP);
+
+  motor->currentSpeed = 0;
+
+  uint sliceA = pwm_gpio_to_slice_num(motor->pinA);
+  pwm_init(sliceA, &config, true);
+  pwm_set_gpio_level(motor->pinA, 0);
+
+  uint sliceB = pwm_gpio_to_slice_num(motor->pinB);
+  pwm_init(sliceB, &config, true);
+  pwm_set_gpio_level(motor->pinB, 0);
 }
 
-void motor_move(Motor motor, int speed)
+static int parse_speed(const Motor *motor, int speed)
 {
-  if (speed == 0)
+  if (abs(speed) <= DEADZONE)
+    return 0;
+
+  if (speed > MAX_SPEED)
+    speed = MAX_SPEED;
+
+  if (speed < -MAX_SPEED)
+    speed = -MAX_SPEED;
+
+  if (motor->reversed)
   {
-    pwm_set_gpio_level(motor.pinA, 0);
-    pwm_set_gpio_level(motor.pinB, 0);
-    return;
+    return -speed;
   }
 
-  uint highPin = speed > 0 ? motor.pinA : motor.pinB;
-  uint lowPin = speed > 0 ? motor.pinB : motor.pinA;
+  return speed;
+}
 
-  if (motor.reversed)
+void dead_time(Motor *motor, int speed)
+{
+  int old_speed = motor->currentSpeed;
+  bool old_dir = old_speed >= 0;
+  bool new_dir = speed >= 0;
+
+  if (old_speed != 0 && old_dir != new_dir)
   {
-    uint temp = lowPin;
-    lowPin = highPin;
-    highPin = temp;
+    pwm_set_gpio_level(motor->pinA, 0);
+    pwm_set_gpio_level(motor->pinB, 0);
+
+    sleep_us(DEADTIME_US);
+  }
+}
+
+void motor_move(Motor *motor, int speed)
+{
+  speed = parse_speed(motor, speed);
+
+  dead_time(motor, speed);
+
+  if (speed > 0)
+  {
+    pwm_set_gpio_level(motor->pinB, 0);
+    pwm_set_gpio_level(motor->pinA, speed);
+  }
+  else if (speed < 0)
+  {
+    pwm_set_gpio_level(motor->pinA, 0);
+    pwm_set_gpio_level(motor->pinB, -speed);
+  }
+  else
+  {
+    pwm_set_gpio_level(motor->pinA, 0);
+    pwm_set_gpio_level(motor->pinB, 0);
   }
 
-  pwm_set_gpio_level(highPin, abs(speed));
-  pwm_set_gpio_level(lowPin, 0);
+  motor->currentSpeed = speed;
 }
